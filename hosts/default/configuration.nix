6b7762e2aaffa4a58582710165f30cace941f87c
Nyct-exe <lukas.krasauskis123@gmail.com>
2024-02-12 14:40:21 +0200

    Added wine support

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -103,6 +103,26 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+     # support both 32- and 64-bit applications
+    wineWowPackages.stable
+
+    # support 32-bit only
+    wine
+
+    # support 64-bit only
+    (wine.override { wineBuild = "wine64"; })
+
+    # support 64-bit only
+    wine64
+
+    # wine-staging (version with experimental features)
+    wineWowPackages.staging
+
+    # winetricks (all versions)
+    winetricks
+
+    # native wayland support (unstable)
+    wineWowPackages.waylandFull
   ];
   
   # Enabling Programs
